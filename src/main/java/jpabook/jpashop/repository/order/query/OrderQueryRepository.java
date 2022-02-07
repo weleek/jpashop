2181621642f85119441b7e66806441ec82e715ea
Jade-Lee85 <weleek@epiens.com>
2022-02-07 15:25:35 +0900

    fix: 주문상품 조회 오타 수정

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -83,7 +83,7 @@ public class OrderQueryRepository {
 
     public List<OrderFlatDto> findAllByDto_flat() {
         return em.createQuery("select new " +
-                "jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderedAt, o.status, d.address, i.name, oi.orderPrice, oi.orderCount)" +
+                "jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderedAt, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
                 " from Order o" +
                 " join o.member m" +
                 " join o.delivery d" +
