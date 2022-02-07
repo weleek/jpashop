0c731e4970eb180ce7e8338ea53a63f8581f13d5
Jade-Lee85 <weleek@epiens.com>
2022-02-07 14:28:07 +0900

    feat: 주문 조회 v4 완료

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,5 +16,14 @@ public class OrderQueryDto {
     private LocalDateTime orderedAt;
     private OrderStatus orderStatus;
     private Address address;
-    private List<OrderItemQueryDto> orderItems;
+    private List<OrderItemQueryDto> orderItems = new ArrayList<>();
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderedAt, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderedAt = orderedAt;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+
 }
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -13,8 +13,39 @@ public class OrderQueryRepository {
 
     private final EntityManager em;
 
+    /**
+     * MyBatis의 resultMap 선언 하여 클래스와 결과값 정의 한 뒤 association이나 collection 이용하는것과 같은 방식
+     */
 
     public List<OrderQueryDto> findOrderQueryDtos() {
-        return new ArrayList<>();
+        List<OrderQueryDto> result = findOrders();
+
+        result.forEach(o -> {
+            List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
+            o.setOrderItems(orderItems);
+        });
+
+        return result;
+    }
+
+    private List<OrderItemQueryDto> findOrderItems(Long orderId) {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id = :orderId",
+                        OrderItemQueryDto.class)
+                .setParameter("orderId", orderId)
+                .getResultList();
+    }
+
+    private List<OrderQueryDto> findOrders() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderedAt, o.status, d.address)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d",
+                OrderQueryDto.class)
+                .getResultList();
     }
 }
